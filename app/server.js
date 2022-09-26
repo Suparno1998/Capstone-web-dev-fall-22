ed2a9f5642e9fdb4aec9acbaa8c53d1377dae371
Suparno1998 <ssuparno1998@gmail.com>
2022-09-26 18:10:30 -0400

    debug

--- app/server.js
+++ app/server.js
@@ -8,7 +8,7 @@ const app = express()
 const PORT = process.env.PORT 
 const DB_URL = process.env.mode === "DEV" ? "mongodb://localhost:27017/capstonedb": process.env.DB_URL
 app.use(express.static(process.env.BUILD_PATH))
-
+logger.info(process.env.PORT, process.env.DB_URL, process.env.BUILD_PATH)
 //testing endpoint
 app.get('/check',(req,res)=>{
     try{
