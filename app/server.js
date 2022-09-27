60d9ba54ccfdbb7524bf0dc4ccbd9f28feb77581
Suparno1998 <ssuparno1998@gmail.com>
2022-09-27 11:21:04 -0400

    production env variables

--- app/server.js
+++ app/server.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 //logger.info(process.env.PORT, process.env.DB_URL, process.env.BUILD_PATH)
 const app = express()
 const PORT = process.env.PORT ? process.env.PORT : 3000
-const DB_URL = process.env.mode === "DEV" ? "mongodb://localhost:27017/capstonedb": "mongodb+srv://root:xFNqHuDfLCRbbpKy@cluster.nfwhf.mongodb.net/capstonedb?retryWrites=true&w=majority" //process.env.DB_URL
+const DB_URL = process.env.mode === "DEV" ? "mongodb://localhost:27017/capstonedb": process.env.DB_URL //process.env.DB_URL
 app.use(express.static("./frontend/public"))
 
 //testing endpoint
