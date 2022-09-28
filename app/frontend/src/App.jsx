b72f931f742813005f7d24eae6ec7eee699690d1
Suparno1998 <ssuparno1998@gmail.com>
2022-09-28 11:09:01 -0400

    test

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -18,6 +18,7 @@ export default class App extends React.Component{
     }
     render(){
         return <div>
+                    <h2>Hello, world!</h2>
                     <h1>Hello, {this.state.team.join(", ")}</h1>
                 </div>
     }
