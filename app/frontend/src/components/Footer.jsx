67229a084d475c8253a36fa157c1f03d8c794cff
rupesh401 <gautamrupesh401@gmail.com>
2022-10-10 18:50:29 -0400

    social media links and footer updated

--- app/frontend/src/components/Footer.jsx
+++ app/frontend/src/components/Footer.jsx
@@ -2,36 +2,58 @@ import React from "react";
 import "../styles/main.css";
 
 function Footer() {
-    return(
-        <footer>
-            <div class="footer container">
-                <div class="footer-info">
-                    <h4>Address</h4>
-                    <span>10 Mill Creek Road, Cambridge</span>
-                </div>
-                
-                <div class="footer-info">
-                    <h4>Office Hours</h4>
-                    <span>Mon-Fri 11am-6pm</span>
-                </div>
-                
-                <div class="footer-info">
-                    <h4>Email</h4>
-                    <a href="mailto:mshaikh@conestogac.on.ca">info@info.com</a>
-                </div>
-                
-                <div class="footer-info">
-                    <h4>Telephone</h4>
-                    <a href="tel:+15197711379">+1 519 771 1379</a>
-                </div>
-            </div>
-            <div class="copyright">
-                <div class="container">
-                    <p>&copy; Capstone Project Group 2 - 2022</p>
-                </div>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div class="footer container">
+        <div class="footer-info">
+          <h4>Address</h4>
+          <span>10 Mill Creek Road, Cambridge</span>
+        </div>
+
+        <div class="footer-info">
+          <h4>Email</h4>
+          <a href="mailto:mshaikh@conestogac.on.ca">info@info.com</a>
+        </div>
+
+        <div class="footer-info">
+          <h4>Telephone</h4>
+          <a href="tel:+15197711379">+1 519 771 1379</a>
+        </div>
+        <div class="footer-info-media">
+          <h4>Social Media Links</h4>
+          <img
+            width={90}
+            className="facebook"
+            src="/images/facebook.png"
+            alt="Facebook Logo"
+          />
+          <img
+            width={90}
+            className="instagram"
+            src="/images/instagram.png"
+            alt="Instagram Logo"
+          />
+          <img
+            width={90}
+            className="twitter"
+            src="/images/twitter.png"
+            alt="Twitter Logo"
+          />
+          <img
+            width={90}
+            className="linkedIn"
+            src="/images/linkedIn.png"
+            alt="LinkedIn Logo"
+          />
+        </div>
+      </div>
+      <div class="copyright">
+        <div class="container">
+          <p>&copy; Capstone Project Group 2 - 2022</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
