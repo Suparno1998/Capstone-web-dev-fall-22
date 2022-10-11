c39ae7f14e9dcae2fc6086862a2d8530db18355e
rupesh401 <gautamrupesh401@gmail.com>
2022-10-11 18:55:01 -0400

    logo and website look color

--- app/frontend/src/components/Navbar.jsx
+++ app/frontend/src/components/Navbar.jsx
@@ -4,34 +4,40 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import "../styles/main.css";
 
 function Navbar(props) {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	return (
-		<header>
-			<div class="container topbar">
-				<h3>LOGO</h3>
-				<nav ref={navRef}>
-					<a href="/#">Home</a>
-					<a href="/#">Plans</a>
-					<a href="/#">Recipes</a>
-					<a href="/#">Contact Us</a>
-					<a href="/#" onClick={props.handleOpen}>Login / Register</a>
-					<button
-						className="nav-btn nav-close-btn"
-						onClick={showNavbar}>
-						<FaTimes />
-					</button>
-				</nav>
-				<button className="nav-btn" onClick={showNavbar}>
-					<FaBars />
-				</button>
-			</div>
-		</header>
-	);
+  return (
+    <header>
+      <div class="container topbar">
+        <a href="">
+          <img
+            width={160}
+            src="/images/foodlablogonew.png"
+            alt="FoodLab Logo"
+          />
+        </a>
+        <nav ref={navRef}>
+          <a href="/#">Home</a>
+          <a href="/#">Plans</a>
+          <a href="/#">Recipes</a>
+          <a href="/#">Contact Us</a>
+          <a href="/#" onClick={props.handleOpen}>
+            Login / Register
+          </a>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars style={{ color: "#28a745" }} />
+        </button>
+      </div>
+    </header>
+  );
 }
 
 export default Navbar;
