72eacf9566e28ddf359d6364bca25274a23230e7
mohmedsajid <sajju123@live.in>
2022-10-31 19:41:07 -0400

    Newsletter subscribers and registered users list page design

--- app/frontend/src/pages/UserProfile/UserProfile.jsx
+++ app/frontend/src/pages/UserProfile/UserProfile.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import NewsletterSubscribers from "../../components/NewsletterSubscribers/NewsletterSubscribers.jsx";
+import RegisteredUsers from "../../components/RegisteredUsers/RegisteredUsers.jsx";
 import UserProfile from "../../components/UserProfile/UserProfile";
 //const profile =  require('../../../../models/UserProfile')
 
 const UserProfiles = () => {
   return (
     <>
+      {/* <NewsletterSubscribers/>
+      <RegisteredUsers/> */}
       <UserProfile />
     </>
   );
