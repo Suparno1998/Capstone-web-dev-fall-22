f6c086e38f46dd156a152c45de8a933687d9ce34
rupesh401 <gautamrupesh401@gmail.com>
2022-11-01 20:10:01 -0400

    admin navbar design
    updated

--- app/frontend/src/pages/LandingPage.jsx
+++ app/frontend/src/pages/LandingPage.jsx
@@ -6,37 +6,39 @@ import HeroHomePage from "../components/HeroHomePage.jsx";
 import Newsletter from "../components/Newletter.jsx";
 import Footer from "../components/Footer.jsx";
 import Modal from "react-bootstrap/Modal";
+import MealPlan from "./MealPlan/Mealplan.jsx";
 
-
-export default function LandingPage(){
-  const message = window.location.search === "" ? "" : window.location.search.replace("?","").split("=")[1]
-  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false)
+export default function LandingPage() {
+  const message =
+    window.location.search === ""
+      ? ""
+      : window.location.search.replace("?", "").split("=")[1];
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
   function handleOpen() {
     console.log("hi");
-    setIsLoginModalOpen(true)
+    setIsLoginModalOpen(true);
   }
   function handleClose() {
-   setIsLoginModalOpen(false)
+    setIsLoginModalOpen(false);
   }
-  useEffect(()=>{
-    console.log(message)
-    if(message === "verified"){
-      console.log('works')
-      alert("Your email was verified successfully. Please login to continue")
+  useEffect(() => {
+    console.log(message);
+    if (message === "verified") {
+      console.log("works");
+      alert("Your email was verified successfully. Please login to continue");
     }
-  },[])
+  }, []);
   return (
-      <div>
-        {/* <Navbar
+    <div>
+      {/* <Navbar
           handleOpen={handleOpen}
           handleClose={handleClose}
         ></Navbar> */}
-        {/* <h2>Hello, world!</h2>
+      {/* <h2>Hello, world!</h2>
                     <h1>Hello, {this.state.team.join(", ")}</h1> */}
-        <HeroHomePage />
-        <Newsletter />
-        
-      </div>
-    );
+      <HeroHomePage />
+      <MealPlan />
+      <Newsletter />
+    </div>
+  );
 }
-
