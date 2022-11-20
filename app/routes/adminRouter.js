c434f2274dea0c4f04159db47f74ee91a6e21daa
rupesh401 <gautamrupesh401@gmail.com>
2022-11-20 16:07:26 -0500

    Contact Message Updated
    Contact Message API created
    Contact Message is shown in admin panel

--- app/routes/adminRouter.js
+++ app/routes/adminRouter.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const adminRouter = express.Router();
 const { MessageModel } = require("../models/Message");
+const { UserModel } = require("../models/User");
 
 adminRouter.get("/messages", (req, res) => {
   MessageModel.find({}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+      console.log("This is error", err);
+    } else {
+      res.status(200).send(data);
+      console.log("This is data", data);
+    }
+  });
+});
+
+adminRouter.get("/orders", (req, res) => {
+  UserModel.find({}, (err, data) => {
     if (err) {
       res.status(500).send(err);
       console.log("This is error", err);
