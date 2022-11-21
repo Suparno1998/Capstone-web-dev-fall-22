04445868fdb68a52d23c9df467328f33f4d5d868
rupesh401 <gautamrupesh401@gmail.com>
2022-11-21 01:02:38 -0500

    register user
    updated

--- app/frontend/src/components/RegisteredUsers/RegisteredUsers.jsx
+++ app/frontend/src/components/RegisteredUsers/RegisteredUsers.jsx
@@ -1,97 +1,45 @@
-
-import React, { Fragment, useEffect, useState } from "react";
-import { Form, Button, Alert } from "react-bootstrap"
-import axios from "../../../../routes/axios"
+import React, { useEffect, useState } from "react";
+import axios from "../../../../routes/axios";
 import "./RegisteredUsers.css";
 
-function RegisteredUsers(){
-    const [userList, setUserList] = useState([]);
+function RegisteredUsers() {
+  const [userLists, setUserLists] = useState("");
 
-    useEffect(() => {
-        const fetchusers = async () => {
-          const userlist = await axios.get("/admin/users-list");
-          console.log("userlist >>>>>", userlist);
-          setUserList(userlist);
-        };
-        fetchusers();
-      }, []);
+  useEffect(() => {
+    const fetchusers = async () => {
+      const data = await axios.get("/admin/users-list");
+      console.log("userlist >>>>>", data);
+      setUserLists(data);
+    };
+    fetchusers();
+  }, []);
 
-    
-    console.log(JSON.stringify(userList));
-    return(
-        <div className="register-user-list">
-            <h1>Registered Users</h1>
-            <table className="table table-striped">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Firstname</th>
-                        <th scope="col">Lastname</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Status</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>mark@mdo.com</td>
-                        <td>Active</td>
-                        <td><a className="btn btn-danger">De Activate</a></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>jacob@fat.com</td>
-                        <td>De-Active</td>
-                        <td><a className="btn btn-primary">Activate</a></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>larry@twitter.com</td>
-                        <td>Active</td>
-                        <td><a className="btn btn-danger">De Activate</a></td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className="row d-flex">
-              <div className="list">
-              <table className="table table-striped">
+  return (
+    <div className="register-user-list">
+      <h1>Registered Users</h1>
+      <table className="table table-striped">
         <thead>
           <tr>
-            <th>ID</th>
+            <th scope="col">#</th>
             <th scope="col">Email</th>
             <th scope="col">Role</th>
-            <th>Action</th>
           </tr>
         </thead>
         <tbody>
-          {userList &&
-            userList?.userList.map((user) => {
+          {userLists &&
+            userLists?.map((u) => {
               return (
                 <tr>
-                  <td>{user._id}</td>
-                  <td>{user.email}</td>
-                  <td>{user.role}</td>
-                  <td>
-                    <a className="btn btn-danger" href="#">
-                      Delete
-                    </a>
-                  </td>
+                  <td>{u._id}</td>
+                  <td>{u.email}</td>
+                  <td>{u.role}</td>
                 </tr>
               );
             })}
         </tbody>
       </table>
-              </div>
-            </div>
-        </div>
-    )
+    </div>
+  );
 }
 
 export default RegisteredUsers;
--- app/routes/adminRouter.js
+++ app/routes/adminRouter.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const adminRouter = express.Router();
 const { MessageModel } = require("../models/Message");
-<<<<<<< HEAD
-const {UserModel} = require("../models/User")
-=======
 const { UserModel } = require("../models/User");
->>>>>>> 509449db557b552661536ee03d2e9cdeb86b3061
 
 adminRouter.get("/messages", (req, res) => {
   MessageModel.find({}, (err, data) => {
@@ -19,19 +15,9 @@ adminRouter.get("/messages", (req, res) => {
   });
 });
 
-adminRouter.get("/orders", (req, res) => {
-  UserModel.find({}, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-      console.log("This is error", err);
-    } else {
-      res.status(200).send(data);
-      console.log("This is data", data);
-    }
-  });
 adminRouter.get("/users-list", async (req, res) => {
   try {
-    console.log("$$$$$$$$$$$$$$")
+    console.log("$$$$$$$$$$$$$$");
     const userList = await UserModel.find();
     console.log(userList);
     //await userprofile.save()
@@ -43,4 +29,4 @@ adminRouter.get("/users-list", async (req, res) => {
   }
 });
 
-module.exports =  {adminRouter} 
+module.exports = { adminRouter };
